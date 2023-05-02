71269605c4ed13ee0c82b346a34fe43070b45b5a
Ronie083 <ahmedronie6@gmail.com>
2023-05-03 02:11:14 +0600

    clint side initialised

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCe-u72GDqCzdRSZj_7qLKc2U5GqkstDC8",
+  authDomain: "chatgaiya-hana.firebaseapp.com",
+  projectId: "chatgaiya-hana",
+  storageBucket: "chatgaiya-hana.appspot.com",
+  messagingSenderId: "834956619604",
+  appId: "1:834956619604:web:8640386843120655da62c1"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
